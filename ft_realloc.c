7d444ec5b307b345a5f389963b18668d44e71b44
saarikoski-jules <jsaariko@student.codam.nl>
2020-10-22 16:51:34 +0200

    protected malloc in reallov

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/21 13:27:22 by jsaariko      #+#    #+#                 */
-/*   Updated: 2020/10/22 09:54:59 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/10/22 16:51:15 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_realloc(void *ptr, size_t size)
 	void *new;
 
 	new = malloc(size);
+	if (new == NULL)
+		return (NULL);
 	if (ptr == NULL)
 		return (new);
 	ft_memmove(new, ptr, size - 1);
